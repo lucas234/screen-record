dbf41001d5c214ccdefdf82157cbbe89afcf41d6
lucas234 <ly_liubo@163.com>
2019-05-04 19:56:41 +0800

    Create singleton.py

---
+++ singleton.py
@@ -0,0 +1,27 @@
+# _*_ coding=utf-8 _*_
+import threading
+
+
+class Singleton(object):
+    _instance_lock = threading.Lock()
+
+    def __init__(self):
+        pass
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(Singleton, "_instance"):
+            with Singleton._instance_lock:
+                if not hasattr(Singleton, "_instance"):
+                    Singleton._instance = object.__new__(cls)
+        return Singleton._instance
+
+
+def singleton(cls):
+    _instance = {}
+
+    def _singleton(*args, **kargs):
+        if cls not in _instance:
+            _instance[cls] = cls(*args, **kargs)
+        return _instance[cls]
+
+    return _singleton
